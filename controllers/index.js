484030ab85e1ff7ef5e4ff44fc3dc6d31c4dd742
Aram Ambartsumyan <aram@Arams-MacBook-Air.local>
2023-02-28 01:27:02 -0800

    server

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const homeRoutes = require('./homeRoutes.js');
+const dashboardRoutes = require('./dashboardRoutes.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
@@ -10,7 +10,5 @@ router.use('/dashboard', dashboardRoutes);
 router.use((req, res) => {
   res.status(404).end();
 });
-
-
 
 module.exports = router;
